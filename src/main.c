df02b8e86ddb6dd8ce7fb5823d67aa5d9d7ac3ca
larisueiro <larissa.sueiro@gmail.com>
2026-08-11 12:41:02 -0300

    Carrinho andando (agr de verdade)

--- src/main.c
+++ src/main.c
@@ -2,85 +2,80 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
 
-// Definição dos pinos
-#define PIN_IN1 4   // PTD4 	- Motor Esquerdo Horário
-#define PIN_IN2 12  // PTA12 	- Motor Esquerdo Anti-Horário
-#define PIN_IN3 5   // PTA5 	- Motor Direito Horário
-#define PIN_IN4 4   // PTA4 	- Motor Direito Anti-Horário
+const struct device *gpio_c;
 
-static const struct device *gpioa_dev;
-static const struct device *gpiod_dev;
+/* PINOS DA PONTE H:
+   0 - Esquerda frente 
+   7 - Esquerda trás 
+
+   3 - Direita frente 
+   4 - Direita trás  */
+
+void parar(void)
+{
+    gpio_pin_set(gpio_c, 7, 0);
+    gpio_pin_set(gpio_c, 0, 0);
+
+    gpio_pin_set(gpio_c, 3, 0);
+    gpio_pin_set(gpio_c, 4, 0);
+}
 
 void frente(void)
 {
-    // Esquerdo Avança
-    gpio_pin_set_raw(gpiod_dev, PIN_IN1, 1);
-    gpio_pin_set_raw(gpioa_dev, PIN_IN2, 0);
-    // Direito Avança
-    gpio_pin_set_raw(gpioa_dev, PIN_IN3, 1);
-    gpio_pin_set_raw(gpioa_dev, PIN_IN4, 0);
+    gpio_pin_set(gpio_c, 7, 0);
+    gpio_pin_set(gpio_c, 0, 1);
+    
+    gpio_pin_set(gpio_c, 3, 1);
+    gpio_pin_set(gpio_c, 4, 0);
 }
 
 void tras(void)
 {
-    // Esquerdo Recua
-    gpio_pin_set_raw(gpiod_dev, PIN_IN1, 0);
-    gpio_pin_set_raw(gpioa_dev, PIN_IN2, 1);
-    // Direito Recua
-    gpio_pin_set_raw(gpioa_dev, PIN_IN3, 0);
-    gpio_pin_set_raw(gpioa_dev, PIN_IN4, 1);
-}
+    gpio_pin_set(gpio_c, 7, 1);
+    gpio_pin_set(gpio_c, 0, 0);
 
-void esquerda(void)
-{
-    // Esquerdo Recua
-    gpio_pin_set_raw(gpiod_dev, PIN_IN1, 0);
-    gpio_pin_set_raw(gpioa_dev, PIN_IN2, 1);
-    // Direito Avança
-    gpio_pin_set_raw(gpioa_dev, PIN_IN3, 1);
-    gpio_pin_set_raw(gpioa_dev, PIN_IN4, 0);
+    gpio_pin_set(gpio_c, 3, 0);
+    gpio_pin_set(gpio_c, 4, 1);
 }
 
 void direita(void)
 {
-    // Esquerdo Avança
-    gpio_pin_set_raw(gpiod_dev, PIN_IN1, 1);
-    gpio_pin_set_raw(gpioa_dev, PIN_IN2, 0);
-    // Direito Recua
-    gpio_pin_set_raw(gpioa_dev, PIN_IN3, 0);
-    gpio_pin_set_raw(gpioa_dev, PIN_IN4, 1);
+    gpio_pin_set(gpio_c, 7, 0);
+    gpio_pin_set(gpio_c, 0, 1);
+
+    gpio_pin_set(gpio_c, 3, 0);
+    gpio_pin_set(gpio_c, 4, 1);
 }
 
-void parar(void)
+void esquerda(void)
 {
-	// Para os dois motores
-    gpio_pin_set_raw(gpiod_dev, PIN_IN1, 0);
-    gpio_pin_set_raw(gpioa_dev, PIN_IN2, 0);
-    gpio_pin_set_raw(gpioa_dev, PIN_IN3, 0);
-    gpio_pin_set_raw(gpioa_dev, PIN_IN4, 0);
+    gpio_pin_set(gpio_c, 7, 0);
+    gpio_pin_set(gpio_c, 0, 1);
+
+    gpio_pin_set(gpio_c, 3, 1);
+    gpio_pin_set(gpio_c, 4, 0);
 }
 
 int main(void)
 {
-	// Verifica se os periféricos estão prontos
-    gpioa_dev = DEVICE_DT_GET(DT_NODELABEL(gpioa));
-    gpiod_dev = DEVICE_DT_GET(DT_NODELABEL(gpiod));
-    if (!device_is_ready(gpioa_dev) || !device_is_ready(gpiod_dev)) {
-        printk("Erro: Controladores GPIO nao estao prontos!\n");
-        return 0;
+    printk("Iniciando programa...\n");
+
+    gpio_c = DEVICE_DT_GET(DT_NODELABEL(gpioc));
+    if (gpio_c == NULL || !device_is_ready(gpio_c))
+    {
+        printk("Erro ao acessar GPIOC!\n");
+        return -1;
     }
 
-    // Configura os pinos como saída
-    gpio_pin_configure(gpiod_dev, PIN_IN1, GPIO_OUTPUT_INACTIVE);
-    gpio_pin_configure(gpioa_dev, PIN_IN2, GPIO_OUTPUT_INACTIVE);
-    gpio_pin_configure(gpioa_dev, PIN_IN3, GPIO_OUTPUT_INACTIVE);
-    gpio_pin_configure(gpioa_dev, PIN_IN4, GPIO_OUTPUT_INACTIVE);
+    gpio_pin_configure(gpio_c, 7, GPIO_OUTPUT_INACTIVE);
+    gpio_pin_configure(gpio_c, 0, GPIO_OUTPUT_INACTIVE);
+    gpio_pin_configure(gpio_c, 3, GPIO_OUTPUT_INACTIVE);
+    gpio_pin_configure(gpio_c, 4, GPIO_OUTPUT_INACTIVE);
 
-	printk("Carrinho pronto! Movendo para FRENTE...\n");
-    frente();
+    parar();
 
-	// Loop infinito
-    while (1) {
+    while (1)
+    {
         k_msleep(1000);
     }
 
